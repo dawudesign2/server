a55f43c8300796c99f2105e9b90e8db028f173cd
Dawudesign <contact@dawudesign.fr>
2022-06-11 11:49:03 +0200

    update 1106

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const session = require('express-session');
+const route = require('./src/routes/index');
+const path = require('path');
 const app = express();
 const port = 7700;
 
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/views'));
 // chemin du dossier public
 app.use(express.static('public'));
 // recupere le fichier json et le transforme en objet
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-// chemin du dossier views
-app.get('/', (req, res) => res.sendFile('views/index.html', { root: __dirname }));
-
-app.use((req, res) => {
-    res.status(404).sendFile('views/404.html', { root: __dirname });
-});
+app.use('/', route );
+app.use((req, res) => res.status(404).render('404'));
 
 // ecoute le port et affiche le message
 app.listen(port, () => {
